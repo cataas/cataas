9e5dec2c9f9c23838aab2e9ef485cc6b9b9b3817
Kevin Balicot <kevinbalicot@gmail.com>
2023-10-19 18:35:45 +0200

    fix: Catch error for analytics

--- modules/cataas/analytics.js
+++ modules/cataas/analytics.js
@@ -3,22 +3,26 @@ const config = require('../../src/shared/configuration/configurator')
 module.exports = {
   sendAnalytic (req) {
     if (req.headers['user-agent']) {
-      fetch('https://analytics.boutdecode.fr/api/send', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'User-Agent': req.headers['user-agent']
-        },
-        body: JSON.stringify({
-          payload: {
-            website: config.get('analytics.websiteId', process.env.ANALYTICS_ID),
-            url: req.url,
-            referrer: req.headers.referer || '',
-            hostname: req.headers.host
+      try {
+        fetch('https://analytics.boutdecode.fr/api/send', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'User-Agent': req.headers['user-agent']
           },
-          type: 'event'
+          body: JSON.stringify({
+            payload: {
+              website: config.get('analytics.websiteId', process.env.ANALYTICS_ID),
+              url: req.url,
+              referrer: req.headers.referer || '',
+              hostname: req.headers.host
+            },
+            type: 'event'
+          })
         })
-      })
+      } catch (e) {
+        console.error(e)
+      }
     }
   }
 }
